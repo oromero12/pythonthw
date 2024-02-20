bd60a06a03aee5628150ab1eec58de22df06b799
oscar.romero.villa <oscar.romero.villa@gmail.com>
2024-02-19 20:55:53 -0500

    ex2

--- ex2.py
+++ ex2.py
@@ -1,3 +1,7 @@
-print("I like typing this")
-print("This is fun")
-print("Yay! Printing.")
+# a comment, this is so you can read you program later.
+# anything after # is ignored in python.
+print("I  could have code like this.")
+# and the comment after is ignored.
+# you can also use a comment to 'disable' or comment out code:
+# print("this is won't run")
+print("this will run")
