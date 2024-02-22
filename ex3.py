7b4183ddcd594a3341a6ab8817961cd902eaee16
oscar.romero.villa <oscar.romero.villa@gmail.com>
2024-02-22 11:00:09 -0500

    ejercicio 3

---
+++ ex3.py
@@ -0,0 +1,12 @@
+print("Ahora contare mis pollos:")
+
+print("Hens", 25+30/6)
+print("Roosters", 100-25*3%4)
+
+print("Ahora contaré mis huevos:")
+
+print(3+2+1-5+4%2-1/4+6)
+
+print("Esto es verdad: 3+2<5-7?")
+
+print(3+2<5-7)
