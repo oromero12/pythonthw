e370c2f164a5fe5aa303882ed49b10792f789ebf
oromero12 <oscar.romero.villa@gmail.com>
2024-03-05 14:44:45 -0500

    ex4

--- ex3.py
+++ ex3.py
@@ -10,3 +10,7 @@ print(3+2+1-5+4%2-1/4+6)
 print("Esto es verdad: 3+2<5-7?")
 
 print(3+2<5-7)
+print("Is ti greater?", 5>-2)
+print("Is it greater or equal?",5>=-2)
+print("Is it less or equal", 5<=-2)
+
